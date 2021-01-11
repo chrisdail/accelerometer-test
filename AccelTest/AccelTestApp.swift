6fb3759e977b91e80bdeb651015c9577ca00c770
Chris Dail <chris@motryx.com>
2021-01-11 08:56:13 -0400

    Initial Commit

---
+++ AccelTest/AccelTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccelTestApp.swift
+//  AccelTest
+//
+//  Created by Chris on 2021-01-11.
+//
+
+import SwiftUI
+
+@main
+struct AccelTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
